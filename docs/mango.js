12e887db50a51ada59a1c82615f672cac8c44bde
niravbansal07 <69631330+niravbansal07@users.noreply.github.com>
2020-10-01 09:30:27 -0300

    Add files via upload

---
+++ docs/mango.js
@@ -0,0 +1,25 @@
+class Mango {
+    constructor(x, y, r) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.r=r;
+      this.body = Bodies.circle(x, y, r, options);
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle);
+      ellipseMode(RADIUS);
+      fill(255);
+      ellipse(0, 0, this.r, this.r);
+      pop();
+    }
+  };
